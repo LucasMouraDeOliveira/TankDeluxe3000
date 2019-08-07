b16b8e7e4cfe554c789236b1785b96c59170d5ff
MatthieuBlm <matthieu.bellamy@hotmail.fr>
2019-08-07 16:24:46 +0200

    Movable tank ok

--- src/main/resources/static/js/draw.js
+++ src/main/resources/static/js/draw.js
@@ -4,10 +4,25 @@ document.addEventListener("DOMContentLoaded", function(){
 
 var fCanvas;
 var fCtx;
+var wheelFrontRight;
 
 function initCanvas() {
 	fCanvas = document.querySelector("#foregroundCanvas");
 	fCtx = fCanvas.getContext("2d");
+	
+	wheelFrontRight = new Image();
+	wheelFrontLeft = new Image();
+	wheelBackRight = new Image();
+	wheelBackLeft = new Image();
+	tankBody = new Image();
+	machineGun = new Image();
+	
+	wheelFrontRight.src = "/assets/img/wheel-front-right-S.png";
+	wheelFrontLeft.src = "/assets/img/wheel-front-left-S.png";
+	wheelBackRight.src = "/assets/img/wheel-back-right-S.png";
+	wheelBackLeft.src = "/assets/img/wheel-back-left-S.png";
+	tankBody.src = "/assets/img/tank-body-S.png";
+	machineGun.src = "/assets/img/machine-gun-S.png";
 }
 
 function drawForeground(gameState) {
@@ -18,6 +33,25 @@ function drawForeground(gameState) {
 	}
 }
 
+// Tank sizing configuration
+var width = 50;
+var height = 50;
+
 function drawTank(x, y) {
 	console.log(x + " " + y);
+	x += 100;			// To remove
+	y += 100;			// To remove
+	
+	
+	fCtx.clearRect(0, 0, fCanvas.width, fCanvas.height);
+//	fCtx.rect(10, 10, 10, 10);
+//	fCtx.stroke();
+	
+	fCtx.drawImage(wheelFrontRight, x + (width / 2), y - (height / 2));
+	fCtx.drawImage(wheelFrontLeft, x - (width / 2), y - (height / 2));
+	fCtx.drawImage(wheelBackRight, x + (width / 2), y + (height / 2));
+	fCtx.drawImage(wheelBackLeft, x - (width / 2), y + (height / 2));
+	fCtx.drawImage(tankBody, x - (width / 2) + 5, y - 15);
+	fCtx.drawImage(machineGun, x - 4, y - 30);
+	
 }
