424d4a647bf20e73ea4da852387a864b6bced23c
MatthieuBlm <matthieu.bellamy@isabo.ai>
2019-09-05 18:58:53 +0200

    Add dyn4j thrust sample

---
+++ src/main/java/com/isabo/battletank/dyn4j/ThrustLauncher.java
@@ -0,0 +1,11 @@
+package com.isabo.battletank.dyn4j;
+
+public class ThrustLauncher {
+
+	public static void main(String[] args) {
+		Thrust simulation = new Thrust();
+		simulation.run();
+
+	}
+
+}
