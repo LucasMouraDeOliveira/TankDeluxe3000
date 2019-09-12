5d6683e7bcc86e4aa11264f789b7503b6b90d09e
MatthieuBlm <matthieu.bellamy@hotmail.fr>
2019-09-12 22:23:26 +0200

    Make level builder editor

---
+++ src/main/resources/static/js/editor.js
@@ -0,0 +1,101 @@
+// Variable
+var fCanvas;
+var bCanvas;
+var fCtx;
+var bCtx;
+var ground;
+var obstacle;
+
+var nbBlockX = 1216 / 32;
+var nbBlockY = 800 / 32;
+
+var level;
+
+document.addEventListener("DOMContentLoaded", function(){
+	initCanvas();
+});
+
+function initCanvas() {
+	// Get canvas
+	fCanvas = document.querySelector("#foregroundCanvas");
+	bCanvas = document.querySelector("#backgroundCanvas");
+	fCtx = fCanvas.getContext("2d");
+	bCtx = bCanvas.getContext("2d");
+	
+	// Ground assets
+	ground = new Image();
+	obstacle = new Image();
+	ground.src = "/assets/img/ground-1.jpg";
+	obstacle.src = "/assets/img/wood-S.png";
+	
+	// Draw background once
+	ground.onload = function() {
+		drawBackground();
+		drawGrid();
+	}
+	
+	// Click event
+	fCanvas.addEventListener('mousedown', clickEvent);
+	
+	// Build level
+	level = new Array(nbBlockX);
+	for (var i = 0; i < level.length; i++) {
+		level[i] = new Array(nbBlockY);
+	}
+	
+}
+
+function clickEvent(event) {
+	var x = parseInt(event.offsetX / 32);
+	var y = parseInt(event.offsetY / 32);
+	
+	if(level[x][y]) {
+		level[x][y] = false;
+	} else {
+		level[x][y] = true;
+	}
+	
+	drawForeground();
+	
+	console.log(JSON.stringify(level).replace(/null/g, "false").replace(/\[/g, "{").replace(/\]/g, "}"));
+}
+
+function drawForeground() {
+	fCtx.clearRect(0, 0, fCanvas.width, fCanvas.height);
+	
+	// Level
+	for (var x = 0; x < level.length; x++) {
+		for (var y = 0; y < level[0].length; y++) {
+			if(level[x][y]) {
+				fCtx.drawImage(obstacle, x * 32, y * 32);
+			}
+		}
+	}
+	
+	drawGrid();
+}
+
+function drawGrid() {
+	// x
+	for(var i = 32; i < 1200; i += 32) {
+		fCtx.beginPath();
+		fCtx.lineWidth = "2";
+		fCtx.strokeStyle = "black";
+		fCtx.rect(i, 0, i, 800);
+		fCtx.stroke();
+	}
+	// y
+	for(var j = 32; j < 800; j += 32) {
+		fCtx.beginPath();
+		fCtx.lineWidth = "2";
+		fCtx.strokeStyle = "black";
+		fCtx.rect(0, j, 1200, j);
+		fCtx.stroke();
+	}
+}
+
+function drawBackground() {
+	let groundPattern = bCtx.createPattern(ground, 'repeat');
+	bCtx.fillStyle = groundPattern;
+	bCtx.fillRect(0, 0, bCanvas.width, bCanvas.height);
+}
---
+++ src/main/webapp/editor.jsp
@@ -0,0 +1,39 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Battle Tank</title>
+		
+		<style>
+		
+			#mainDiv {
+				width: 1200px;
+				height: 800px;
+				margin: auto;
+				border: 1px solid black;
+				background-color: white;
+				position: relative;
+			}
+			
+	 		#mainDiv canvas {
+	 			position: absolute;
+	 			top: 0;
+	 			left: 0;
+	 		}
+		
+		</style>
+		
+	</head>
+	<body>
+	
+		<div id="mainDiv">
+			<canvas id="backgroundCanvas" width=1200 height=800></canvas>
+			<canvas id="foregroundCanvas" width=1200 height=800></canvas>
+		</div>
+	
+		<script type="text/javascript" src="js/draw.js"></script>
+		<script type="text/javascript" src="js/editor.js"></script>
+	
+	</body>
+</html>
