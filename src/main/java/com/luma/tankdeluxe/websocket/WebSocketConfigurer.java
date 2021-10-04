09aeed653eb9bb1f51a7b6ae6967d99f46328b7d
Matthieu Bellamy <matthieu.bellamy@buawei.com>
2021-10-04 18:53:18 +0200

    initialize stomp

---
+++ src/main/java/com/luma/tankdeluxe/websocket/WebSocketConfigurer.java
@@ -0,0 +1,36 @@
+package com.luma.tankdeluxe.websocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfigurer implements WebSocketMessageBrokerConfigurer {
+
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry config) {
+		config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/message");
+    }
+	 
+   @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+	   registry.addEndpoint("websockets").setAllowedOrigins("*").withSockJS();
+	}
+   
+   @Override
+   public void configureClientOutboundChannel(ChannelRegistration registration) {
+       registration.taskExecutor().corePoolSize(1);
+   }
+
+   @Override
+   public void configureClientInboundChannel(ChannelRegistration registration) {
+       registration.taskExecutor().corePoolSize(1);
+   }
+	    
+}
+
---
+++ src/main/resources/static/js/websocket-client.js
@@ -0,0 +1,81 @@
+class WebSocketClient {
+	
+	constructor() {
+		let socket = new SockJS('/websockets');
+		
+		this.topicSubscribed = [];
+		this.stompClient = Stomp.over(socket);
+		
+		// Disable logging
+	    this.stompClient.debug = null;
+
+		// Enable auto reconnect
+		this.stompClient.reconnect_delay = 1000;
+		
+		// Connect stompClient
+		this.connect();
+	}
+	
+	connect = () => {
+		this.stompClient.connect({}, this.connectCallback);
+	}
+	
+	// Function called after websocket connected
+	connectCallback = () => {
+		console.log("WebSocket connected");
+		
+		this.topicSubscribed.forEach((topic) => {
+			this.subscribe(topic.topicName, topic.onMessageCallback);
+		});
+	}
+	
+	// Subscribe to the given topic
+	subscribe = (topicName, onMessageCallback) => {
+		
+		// If websocket isn't connected yet
+		if(!this.stompClient.connected) {
+			// Delay subscription from 100ms
+			setTimeout(() => {
+				this.subscribe(topicName, onMessageCallback);
+			}, 100);
+			
+		} else {
+			// Perform subscription
+			let subscription = this.stompClient.subscribe("/topic/" + topicName, onMessageCallback);
+
+			// Save subscribed topic
+			this.topicSubscribed.push({"topicName": topicName, "onMessageCallback": onMessageCallback, "subscription": subscription});
+			
+			console.log("WebSocket subscribed to topic " + topicName);
+		}
+	}
+	
+	// Send a message to the given topic
+	sendMessage = (topicName, message) => {
+		this.stompClient.send("/topic/" + topicName, {}, message);
+			
+		console.log("Message send to topic " + topicName);
+	}
+	
+	// Function to disconnect the websocket
+	disconnect = (topicName) => {
+	    if (this.stompClient !== null) {
+			if(topicName == undefined) {
+				this.stompClient.disconnect();
+
+			    console.log("WebSocket disconnected");
+			} else {
+				let topic = this.topicSubscribed
+									.filter(t => t.topicName == topicName)
+									.pop();
+				if(topic == undefined) {
+					console.log("No topic found with name " + topicName);
+				} else {
+					topic.subscription.unsubscribe();
+
+					console.log("Disconnected from topic " + topicName);
+				}
+			}
+	    }
+	}
+}
